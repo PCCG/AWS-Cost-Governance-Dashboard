cbbb17f5be1d6b78630f81ae07c6a1d3ccfd994a
Ajith Kumar S <aka434112@gmail.com>
2020-06-08 14:05:42 +0530

    feat(accounts): The form is now a part of a dialog box

--- src/components/Accounts/AwsAccountForm.vue
+++ src/components/Accounts/AwsAccountForm.vue
@@ -1,36 +1,31 @@
 <template>
-  <v-container fluid class="component-within-sfc">
+  <v-container fluid>
     <v-layout row wrap>
-        <el-card style="width: 100% !important">
-          <header class="card-title">AWS Account Integration  <i @click="showHelpDialogForIntegratingAwsAccounts" class="el-icon-question" style="cursor: pointer;" title="More info"></i></header>
-          <el-form :rules="awsAccountFormrules" :ref="formName" :model="awsAccountForm">
-            <el-form-item label="Access Key ID" prop="accessKeyId">
-              <el-input v-model="awsAccountForm.accessKeyId" name="Access Key ID"></el-input>
-            </el-form-item>
-            <el-form-item label="Secret Access Key" prop="secretAccessKey">
-              <el-input type="password" v-model="awsAccountForm.secretAccessKey" name="Secret Access Key"></el-input>
-            </el-form-item>
-            <el-form-item label="AWS service to integrate with">
-              <el-radio-group v-model="awsAccountForm.awsService">
-                <el-radio v-once :label="COST_EXPLORER_SERVICE"></el-radio>
-                <el-radio v-once :label="COST_AND_USAGE_REPORTS_SERVICE"></el-radio>
-              </el-radio-group>
-            </el-form-item>
-            <el-form-item label="Polling interval (in hours)" prop="pollingInterval">
-              <el-input-number :min="1" controls-position="right" v-model="awsAccountForm.pollingInterval"></el-input-number>
-            </el-form-item>
-            <el-form-item>
-              <el-button type="primary" :loading="loading" @click="validateFormData" name="Integrate AWS account">Submit</el-button>
-            </el-form-item>
-          </el-form>
-        </el-card>
+      <el-form :rules="awsAccountFormrules" :ref="formName" :model="awsAccountForm">
+        <el-form-item label="Access Key ID" prop="accessKeyId">
+          <el-input v-model="awsAccountForm.accessKeyId" name="Access Key ID"></el-input>
+        </el-form-item>
+        <el-form-item label="Secret Access Key" prop="secretAccessKey">
+          <el-input type="password" v-model="awsAccountForm.secretAccessKey" name="Secret Access Key"></el-input>
+        </el-form-item>
+        <el-form-item label="AWS service to integrate with">
+          <el-radio-group v-model="awsAccountForm.awsService">
+            <el-radio v-once :label="COST_EXPLORER_SERVICE"></el-radio>
+            <el-radio v-once :label="COST_AND_USAGE_REPORTS_SERVICE"></el-radio>
+          </el-radio-group>
+        </el-form-item>
+        <el-form-item label="Polling interval (in hours)" prop="pollingInterval">
+          <el-input-number :min="1" controls-position="right" v-model="awsAccountForm.pollingInterval"></el-input-number>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" :loading="loading" @click="validateFormData" name="Integrate AWS account">Submit</el-button>
+        </el-form-item>
+      </el-form>
     </v-layout>
   </v-container>
 </template>
 
 <script>
-import { mapMutations } from 'vuex';
-import helpDialog from '../../constants/helpDialog';
 import accountsClient from '../../rest/accountsClient'
 
 const COST_EXPLORER_SERVICE = 'Cost Explorer (CE)';
@@ -64,15 +59,6 @@ export default {
     }
   },
   methods: {
-    ...mapMutations([
-      'SET_HELP_DIALOG_CONTENTS',
-      'HELP_DIALOG_STATE'
-    ]),
-    showHelpDialogForIntegratingAwsAccounts () {
-      const vm = this;
-      vm.SET_HELP_DIALOG_CONTENTS(helpDialog.getHelpForIntegratingAwsAccounts());
-      vm.HELP_DIALOG_STATE(true);
-    },
     validateFormData () {
       const vm = this;
       vm.$refs[vm.formName].validate((valid) => {
--- src/views/Accounts.vue
+++ src/views/Accounts.vue
@@ -1,17 +1,44 @@
 <template>
   <v-container fluid>
     <v-layout row wrap>
-      <v-flex xs12 sm5>
-        <aws-account-form></aws-account-form>
+      <v-flex xs12 class="text-right">
+        <i class="el-icon-circle-plus clickable-icon" title="Add an AWS account" @click="addAwsAccount = true" role="button"></i>
+        <i class="el-icon-question clickable-icon" title="Help" @click="showHelpDialogForIntegratingAwsAccounts" role="button"></i>
       </v-flex>
+      <el-dialog
+        :title="awsAccountFormTitle"
+        :visible.sync="addAwsAccount"
+        :close-on-click-modal="false"
+        width="40%">
+        <aws-account-form></aws-account-form>
+      </el-dialog>
     </v-layout>
   </v-container>
 </template>
 
 <script>
-import awsAccountForm from '../components/Accounts/AwsAccountForm'
+import { mapMutations } from 'vuex';
+import awsAccountForm from '../components/Accounts/AwsAccountForm';
+import helpDialog from '../constants/helpDialog';
 
 export default {
+  data () {
+    return {
+      addAwsAccount: false,
+      awsAccountFormTitle: 'AWS Account Integration'
+    }
+  },
+  methods: {
+    showHelpDialogForIntegratingAwsAccounts () {
+      const vm = this;
+      vm.SET_HELP_DIALOG_CONTENTS(helpDialog.getHelpForIntegratingAwsAccounts());
+      vm.HELP_DIALOG_STATE(true);
+    },
+    ...mapMutations([
+      'SET_HELP_DIALOG_CONTENTS',
+      'HELP_DIALOG_STATE'
+    ])
+  },
   components: {
     awsAccountForm
   }
@@ -19,5 +46,9 @@ export default {
 </script>
 
 <style scoped>
+
+.el-dialog__body {
+  padding: 0 !important;
+}
 
 </style>
