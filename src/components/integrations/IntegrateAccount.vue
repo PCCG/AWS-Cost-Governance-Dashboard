c174af4090fa52214e0941cb8ff24016976e11fe
Ajith Kumar S <aka434112@gmail.com>
2020-07-27 01:40:50 +0530

    feat(integrations): support multiple providers - initial commit

--- src/components/integrations/IntegrateAccount.vue
+++ src/components/integrations/IntegrateAccount.vue
@@ -1,45 +1,80 @@
 <template>
-  <el-dialog
-    :visible.sync="enableIntegrationDialog"
-    :close-on-click-modal="false"
-    width="55%">
+  <div>
+    <el-dialog
+      :visible="true"
+      @close="cancelIntegration"
+      v-show="getCurrentStep() === 0"
+      :close-on-click-modal="false"
+      width="40%">
       <template v-slot:title>
-        <header class="card-title" style="font-family: 'Montserrat', sans-serif !important;">{{title}}</header>
+        <header class="card-title">Choose a Provider</header>
       </template>
-      <v-container fluid>
-        <v-layout row wrap>
-          <v-flex xs12>
-            <header>{{awsAccountIntegrationSteps[integrationCurrentStep].getDescription()}}</header>
-            <br/>
-          </v-flex>
-          <v-flex xs4>
-            <el-steps :active="getCurrentStep()" direction="vertical" align-center style="margin-top: 20px">
-              <el-step v-for="accountIntegrationStep in awsAccountIntegrationSteps" :key="accountIntegrationStep.getName()" :title="accountIntegrationStep.getName()" :description="accountIntegrationStep.getDescription()"></el-step>
-            </el-steps>
-          </v-flex>
-          <v-flex xs1></v-flex>
-          <v-flex xs7>
-            <aws-account-form :integrationAccountStep="awsAccountIntegrationSteps[1]" v-show="getCurrentStep() === 1">
-              <template v-slot:fields="awsAccountForm">
-                <el-form-item label="Alias Name" prop="aliasName" :rules="[{required: true, message: 'Please specify an Alias', trigger: 'blur'}]">
-                  <el-input v-model="awsAccountForm.awsAccountFormModel.aliasName" name="Alias Name"></el-input>
-                </el-form-item>
-                <el-form-item label="Polling Interval (in hours)" prop="pollingInterval" :rules="[{required: true, message: 'Please specify the Polling Interval', trigger: 'blur'}]">
-                  <el-input-number controls-position="right" name="Polling interval" :min="1" v-model="awsAccountForm.awsAccountFormModel.pollingInterval"></el-input-number>
-                </el-form-item>
-              </template>
-            </aws-account-form>
-            <aws-cur-form :integrationAccountStep="awsAccountIntegrationSteps[2]" v-if="getCurrentStep() === 2" :awsAccount="awsAccountIntegrationSteps[1].getFormData()"/>
-          </v-flex>
-        </v-layout>
-      </v-container>
+      <el-table
+          :show-header="false"
+          :data="supportedProviders"
+          highlight-current-row
+          @current-change="setSelectedProvider"
+          style="width: 100%; font-weight: 600">
+          <el-table-column align="center">
+            <template v-slot="{ row }">
+              <img align="center" height="50px" :src="row.logo" class="image">
+            </template>
+          </el-table-column>
+          <el-table-column
+            align="left"
+            property="name">
+          </el-table-column>
+      </el-table>
       <template v-slot:footer>
-        <el-button type="primary" v-show="isNextStepAvailable" @click="actionToRunOnNext">Next</el-button>
-        <el-button type="primary" v-show="!isNextStepAvailable" name="Integrate AWS account" @click="actionToRunOnNext">Submit</el-button>
-        <el-button type="primary" v-show="getCurrentStep() > 1" @click="previousStep">Back</el-button>
+        <el-button v-show="selectedProvider" type="primary" @click="nextStep()">Next</el-button>
         <el-button class="primary-color" @click="cancelIntegration">Cancel</el-button>
       </template>
     </el-dialog>
+    <el-dialog
+      :visible="true"
+      selectedProvider
+      v-if="getCurrentStep() > 0 && selectedProvider === PROVIDER_AWS_NAME"
+      :close-on-click-modal="false"
+      @close="cancelIntegration"
+      width="55%">
+        <template v-slot:title>
+          <header class="card-title">Integrate AWS Account</header>
+        </template>
+        <v-container fluid>
+          <v-layout row wrap>
+            <v-flex xs12>
+              <header>{{awsAccountIntegrationSteps[integrationCurrentStep].getDescription()}}</header>
+              <br/>
+            </v-flex>
+            <v-flex xs4>
+              <el-steps :active="getCurrentStep()" direction="vertical" align-center style="margin-top: 20px">
+                <el-step v-for="accountIntegrationStep in awsAccountIntegrationSteps" :key="accountIntegrationStep.getName()" :title="accountIntegrationStep.getName()" :description="accountIntegrationStep.getDescription()"></el-step>
+              </el-steps>
+            </v-flex>
+            <v-flex xs1></v-flex>
+            <v-flex xs7>
+              <aws-account-form :integrationAccountStep="awsAccountIntegrationSteps[1]" v-show="getCurrentStep() === 1">
+                <template v-slot:fields="awsAccountForm">
+                  <el-form-item label="Alias Name" prop="aliasName" :rules="[{required: true, message: 'Please specify an Alias', trigger: 'blur'}]">
+                    <el-input v-model="awsAccountForm.awsAccountFormModel.aliasName" name="Alias Name"></el-input>
+                  </el-form-item>
+                  <el-form-item label="Polling Interval (in hours)" prop="pollingInterval" :rules="[{required: true, message: 'Please specify the Polling Interval', trigger: 'blur'}]">
+                    <el-input-number controls-position="right" name="Polling interval" :min="1" v-model="awsAccountForm.awsAccountFormModel.pollingInterval"></el-input-number>
+                  </el-form-item>
+                </template>
+              </aws-account-form>
+              <aws-cur-form :integrationAccountStep="awsAccountIntegrationSteps[2]" v-if="getCurrentStep() === 2" :awsAccount="awsAccountIntegrationSteps[1].getFormData()"/>
+            </v-flex>
+          </v-layout>
+        </v-container>
+        <template v-slot:footer>
+          <el-button type="primary" v-show="isNextStepAvailable" @click="actionToRunOnNext">Next</el-button>
+          <el-button type="primary" v-show="!isNextStepAvailable" name="Integrate AWS account" @click="actionToRunOnNext">Submit</el-button>
+          <el-button type="primary" v-show="getCurrentStep() > 0" @click="previousStep">Back</el-button>
+          <el-button class="primary-color" @click="cancelIntegration">Cancel</el-button>
+        </template>
+      </el-dialog>
+  </div>
 </template>
 
 <script>
@@ -50,18 +85,25 @@ import awsAccountIntegrationSteps from '@/constants/integrations/aws/integration
 import awsAccountForm from './aws/AccountForm'
 import awsCurForm from './aws/CurForm'
 
+const PROVIDER_AWS_NAME = 'Amazon Web Services';
+const PROVIDER_GCP_NAME = 'Google Cloud Platform';
+
 export default {
     data () {
       return {
-        integrationCurrentStep: 1,
+        integrationCurrentStep: 0,
         awsAccount: null,
-        awsAccountIntegrationSteps
+        PROVIDER_AWS_NAME,
+        PROVIDER_GCP_NAME,
+        awsAccountIntegrationSteps,
+        supportedProviders: [
+          {name: PROVIDER_AWS_NAME, logo: require('@/assets/integrations/aws-logo.png')},
+          {name: PROVIDER_GCP_NAME, logo: require('@/assets/integrations/gcp-logo.png')}
+        ],
+        selectedProvider: null
       }
     },
     props: {
-      title: {
-        type: String
-      },
       integrateAccount: {
         type: Boolean
       }
@@ -88,6 +130,10 @@ export default {
       ...mapActions('integrations', [
         'INTEGRATE_AWS_ACCOUNT'
       ]),
+      setSelectedProvider (provider) {
+        const vm = this;
+        vm.selectedProvider = provider.name;
+      },
       actionToRunOnNext: async function () {
         const vm = this;
         const currentStep = vm.getCurrentStep();
--- src/views/Integrations.vue
+++ src/views/Integrations.vue
@@ -2,11 +2,11 @@
   <v-container fluid>
     <v-layout row wrap>
       <v-flex xs9 class="text-left">
-        <header class="title" style="font-family: 'Montserrat', sans-serif !important">AWS INTEGRATIONS</header>
+        <header class="title" style="font-family: 'Montserrat', sans-serif !important">PUBLIC CLOUD INTEGRATIONS</header>
       </v-flex>
       <v-flex xs3 class="text-right">
-        <i class="el-icon-plus primary-color clickable-icon" title="Add an AWS account" @click="integrateAccount = true" role="button"></i>
-        <i class="el-icon-refresh primary-color clickable-icon" title="Refresh" @click="FETCH_AWS_ACCOUNTS" role="button"></i>
+        <i class="el-icon-plus primary-color clickable-icon" title="Add an account" @click="integrateAccount = true" role="button"></i>
+        <i class="el-icon-refresh primary-color clickable-icon" title="Refresh" @click="fetchIntegratedAccounts" role="button"></i>
         <i class="el-icon-help primary-color clickable-icon" title="Help" @click="showHelpDialogForRoute(routeName)" role="button"></i>
       </v-flex>
       <v-flex xs12>
@@ -14,7 +14,7 @@
       </v-flex>
       <!-- The class "component-within-sfc" should be specified in all the components that are a part of "views"-->
       <!-- The statement specified above is not applicable to this component (since it's a dialog) -->
-      <integrate-account v-if="integrateAccount" @close-dialog="integrateAccount = false" :integrateAccount="integrateAccount" :title="integrationDialogTitle"/>
+      <integrate-account v-if="integrateAccount" @close-dialog="integrateAccount = false" :integrateAccount="integrateAccount"/>
     </v-layout>
   </v-container>
 </template>
@@ -43,7 +43,11 @@ export default {
     ]),
     ...mapActions('integrations', [
       'FETCH_AWS_ACCOUNTS',
-    ])
+    ]),
+    fetchIntegratedAccounts () {
+      const vm = this;
+      vm.FETCH_AWS_ACCOUNTS();
+    }
   },
   components: {
     awsAccountsIntegrated,
@@ -60,7 +64,7 @@ export default {
   created () {
     const vm = this;
     vm.routeName = vm.$route.name;
-    vm.FETCH_AWS_ACCOUNTS();
+    vm.fetchIntegratedAccounts();
   }
 }
 </script>
